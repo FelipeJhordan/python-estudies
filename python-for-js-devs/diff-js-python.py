b62b16242085b39a23f4be550a6d5e53e1cede5b
felipejhordan <felipejordan.alves@gmail.com>
2022-06-09 20:25:04 -0300

    feat: python-for-js-dev up

---
+++ python-for-js-devs/diff-js-python.py
@@ -0,0 +1,31 @@
+## Arithmetic operators in Python and JavaScript
+## Floor division only exists in python
+## Floor division return the integer quotient of the division between two numbers
+# floorResult = 89 // 4
+# print(floorResult) # 22
+
+## Python allows you use multiplication on strings to repeat a pattern 
+# "a" * 9
+
+## Nonetheless not allow you sum number and string together (JS allows, because the number is auto cast in string)
+# "a" + 9
+
+## Python is no such thing as a decrement/increment operator 
+## Like as
+# let c = 2
+# c = c++
+
+## Difference Logical operators between js and python
+# OPERATOR	PYTHON	JAVASCRIPT
+# logical and	and	&&
+# logical or	or	||
+# logical negation	not	!
+
+## Complex Data in js and python
+
+
+# PYTHON	JAVASCRIPT	MUTABLE IN PYTHON	MUTABLE IN JS
+# list	        Array	    yes	                    yes
+# dictionary	Object	    yes	                    yes
+# set	           Set	    yes	                    yes
+# tuple	           n/a	    n/a	                    n/a
